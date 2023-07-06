bc6864c4c9ccd93f10f3070f64a50e63c78146e6
Matte <mattias.johansson@funnel.io>
2023-07-06 09:19:58 +0200

    Reservoir now remembers original order and prints results sorted.

--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,24 @@ pub fn main() -> Result<()> {
     reset_sigpipe();
     // rand::
     let args = Args::parse();
-    let mut reservoir: Vec<String> = io::stdin()
+    let mut reservoir: Vec<(usize, String)> = io::stdin()
         .lines()
         .take(args.n)
         .filter_map(std::result::Result::ok)
+        .enumerate()
         .collect();
 
     let mut rng = rand::thread_rng();
     for (i, line) in io::stdin().lines().enumerate() {
-        let j: usize = rng.gen_range(0..i + reservoir.len());
+        let i = i + reservoir.len();
+        let j: usize = rng.gen_range(0..i);
         if j < reservoir.len() {
-            reservoir[j] = line?;
+            reservoir[j] = (i, line?);
         }
     }
+    reservoir.sort_by_key(|x| x.0);
     for item in reservoir {
-        println!("{}", item);
+        println!("{}", item.1);
     }
     Ok(())
 }
